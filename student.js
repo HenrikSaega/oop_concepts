d4d3d409feb4f1cad051b45cd4c23a3fab1f01c4
Henrik <henrik.saega@voco.ee>
2024-11-15 15:43:17 +0200

    Ülesanne 1

--- student.js
+++ student.js
@@ -1,10 +1,30 @@
 class Student{
-    constructor(name, finished=false){
-        this.name = name;
-        this.finished = finished;
+    constructor(firstname, lastname, age){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+    } 
+}
+class Person{
+    constructor(){
+        this.firstname = "";
+        this.lastname = "";
+        this.age = 0;
     } 
 }
 
-const student = new Student('Henrik');
-console.log(student.name);
-console.log(student.finished);
+const person1 = new Person("Juhan", "Mardi", 20);
+const person2 = new Person("Mari", "Liisi", 18);
+const person3 = new Person("Kristo", "Toop", 32);
+
+const student1 = new Student("Henrik", "Saega", 20);
+const student2 = new Student("Laur", "Sulg", 23);
+const student3 = new Student("Annika", "Kimbu", 24);
+
+console.log(person1);
+console.log(person2);
+console.log(person3);
+
+console.log(student1);
+console.log(student2);
+console.log(student3);
